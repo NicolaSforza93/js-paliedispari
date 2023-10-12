2f63b7db4e846fe11c04d33d16af7297a1485ba9
NicolaSforza93 <nicola.sforza93@gmail.com>
2023-10-12 17:40:02 +0200

    added ex. palindr. and added folder pari-dispari

---
+++ README.md
@@ -0,0 +1,16 @@
+## Palindroma
+Chiedere all’utente di inserire una parola Creare una funzione per capire se la parola inserita è palindroma
+
+- chiedo all'utente di inserire una parola 
+  - inizializzo una variabile con la parola inserita dall'utente attraverso il prompt
+- creo una funzione
+  - do un nome alla funzione e inserisco l'argomento parola 
+  - creo una variabile vuota che avrà la parola
+  - apro un ciclo for decrescente
+  - restituisco la parola invertita
+- inizializzo una varibile che contenga la funzione
+- istruzioni condizionali
+  - SE la parola inserita è identica a quella invertita
+    - stampo 'la parola è palindroma'
+  - ALTRIMENTI 
+    - stampo 'la parola non è palindroma'
--- js/main.js
+++ js/main.js
@@ -1,1 +1,58 @@
-console.log('js-paliedispari')
+// console.log('js-paliedispari')
+
+// - chiedo all'utente di inserire una parola 
+//   - inizializzo una variabile con la parola inserita dall'utente attraverso il prompt
+const userWord = prompt('inserire una parola');
+console.log(userWord);
+// - creo una funzione
+//   - do un nome alla funzione e inserisco l'argomento parola 
+function reverseWord(word) {
+//   - creo una variabile vuota che avrà la parola
+    let wordReversed = '';
+    // console.log(word.length);
+//   - apro un ciclo for decrescente
+    for (let i = word.length - 1; i >= 0; i--) {
+        wordReversed += word[i];
+        // console.log(i, wordReversed);
+    }
+//   - restituisco la parola invertita
+    return wordReversed;
+}
+
+// - inizializzo una varibile che contenga la funzione
+const wordBackwards = reverseWord(userWord);
+// - istruzioni condizionali
+//   - SE la parola inserita è identica a quella invertita
+if (userWord === wordBackwards) {
+//     - stampo 'la parola è palindroma'
+    console.log('La parola è palindroma');
+//   - ALTRIMENTI 
+} else {
+//     - stampo 'la parola non è palindroma'
+    console.log('La parola non è palindroma');
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
